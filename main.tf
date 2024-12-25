a7999b8455543d27960a953fb99a790dc7da6c20
Nemanja Maric <nemanja.maric.22@hotmail.com>
2024-12-25 23:05:04 +0100

    Updated

--- main.tf
+++ main.tf
@@ -1,22 +1,15 @@
-#Provider configuration details for Terraform
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.0.0"
+      version = "4.14.0"
     }
   }
 }
 
 provider "azurerm" {
   # Configuration options
-  features {}
-
   subscription_id = "b097d0bc-e8b1-48fe-9a11-2104ff538451"
-}
 
-module "virtual_machine" {
-  source = "./virtual_machine"
-  prefix = "test_prefix"
+  features {}
 }
-
